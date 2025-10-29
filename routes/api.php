2836fb5541aa2a965e032d5ae623cf3b58f4ad62
niltalamal <tataamal1128@gmail.com>
2025-10-29 13:59:48 +0700

    health

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,25 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/health', function () {
+    try {
+        DB::connection()->getPdo();
+        return response()->json([
+            'status' => 'OK',
+            'message' => 'Application and Database are healthy.'
+        ], 200);
+
+    } catch (\Exception $e) {
+        return response()->json([
+            'status' => 'ERROR',
+            'message' => 'Database connection failed.',
+            'error' => $e->getMessage()
+        ], 503);
+    }
+});
